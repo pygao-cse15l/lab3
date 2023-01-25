323a107f6547a9e9f99b4a6d533ba1ba2d591b20
Spacefroyo <petergao322@gmail.com>
2023-01-25 14:39:45 -0800

    Fixed LinkedList

---
+++ LinkedListTest.java
@@ -0,0 +1,29 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class LinkedListTest {
+    @Test
+    public void testMultipleAppend() {
+        LinkedList list = new LinkedList();
+        list.append(0);
+        list.append(1);
+        list.append(2);
+
+        String out = "0 1 2 ";
+
+        assertEquals(out, list.toString());
+    }
+
+
+    @Test
+    public void testMultiple() {
+        LinkedList list = new LinkedList();
+        list.append(0);
+        list.append(1);
+
+        String out = "0 1 ";
+
+        assertEquals(out, list.toString());
+    }
+}
