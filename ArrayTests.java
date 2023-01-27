e3d86b0344d0bd31b75c4bd084e160e9758e83a2
Spacefroyo <petergao322@gmail.com>
2023-01-27 15:37:49 -0800

    done

--- ArrayTests.java
+++ ArrayTests.java
@@ -1,7 +1,5 @@
 import static org.junit.Assert.*;
 
-import java.lang.reflect.Array;
-
 import org.junit.*;
 
 public class ArrayTests {
--- ListTests.java
+++ ListTests.java
@@ -11,10 +11,16 @@ public class ListTests {
         StringChecker input2 = new StringChecker() {
             public boolean checkString(String s) { return true; }
         };
-
         assertArrayEquals(new String[]{"a", "b"}, ListExamples.filter(input1, input2).toArray());
     }
-
+    @Test
+    public void testFilter2() {
+        List<String> input1 = Arrays.asList("a", "a");
+        StringChecker input2 = new StringChecker() {
+            public boolean checkString(String s) { return true; }
+        };
+        assertArrayEquals(new String[]{"a", "a"}, ListExamples.filter(input1, input2).toArray());
+    }
 
     @Test
     public void testMerge() {
