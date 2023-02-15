a22146b7250e3fad79084759f2470ab96bbb1ef4
Spacefroyo <petergao322@gmail.com>
2023-02-15 14:36:40 -0800

    Update ListTests.java

--- ListTests.java
+++ ListTests.java
@@ -4,22 +4,30 @@ import static org.junit.Assert.assertArrayEquals;
 
 import java.util.*;
 
+class PalindromeChecker implements StringChecker {
+    public boolean checkString(String s) {
+        for (int i = 0; i < s.length()/2; i++) {
+            if (s.charAt(i) != s.charAt(s.length()-1-i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
 public class ListTests {
     @Test
     public void testFilter() {
-        List<String> input1 = Arrays.asList("a", "b");
-        StringChecker input2 = new StringChecker() {
-            public boolean checkString(String s) { return true; }
-        };
-        assertArrayEquals(new String[]{"a", "b"}, ListExamples.filter(input1, input2).toArray());
+        List<String> input1 = Arrays.asList("a", "b", "ab", "bcb");
+        StringChecker input2 = new PalindromeChecker();
+
+        assertArrayEquals(new String[]{"a", "b", "bcb"}, ListExamples.filter(input1, input2).toArray());
     }
+    
     @Test
     public void testFilter2() {
-        List<String> input1 = Arrays.asList("a", "a");
-        StringChecker input2 = new StringChecker() {
-            public boolean checkString(String s) { return true; }
-        };
-        assertArrayEquals(new String[]{"a", "a"}, ListExamples.filter(input1, input2).toArray());
+        List<String> input1 = Arrays.asList("a", "ba", "aba", "ca", "a");
+        StringChecker input2 = new PalindromeChecker();
+        assertArrayEquals(new String[]{"a", "aba", "a"}, ListExamples.filter(input1, input2).toArray());
     }
 
     @Test
